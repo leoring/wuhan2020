d923ca8d296567fa08498d25f597ff79f76d4697
leoring <lening@smmail.cn>
2020-02-09 17:58:30 +0800

    改进了模型仿真结果显示曲线

--- Infect_model.py
+++ Infect_model.py
@@ -4,10 +4,10 @@ Created on Tue Feb  4 09:37:50 2020
 Infection model
 By referring following URL:
 https://zhuanlan.zhihu.com/p/104268573?night=1
-@author: LENING
+@author: leoring_le@hotmail.com
 """
 
-import pylab as pl
+import matplotlib.pyplot as plt
 import random
 
 # N 人口总数
@@ -30,17 +30,18 @@ def SI_model(N= 10000, I= 1, r= 10, B= 0.01):
         Suspectors.append(Suspectors[idx] - r*B*Infectors[idx]*Suspectors[idx]/N)
         Infectors.append(Infectors[idx] + r*B*Infectors[idx]*Suspectors[idx]/N)
     
-    pl.plot(Suspectors,  label= u"Susceptibles")
-    pl.plot(Infectors, label= u"Infectors")
-    pl.legend()
-    pl.show()
+    plt.cla()
+    plt.plot(Suspectors,  label= u"Susceptibles")
+    plt.plot(Infectors, label= u"Infectors")
+    plt.pause(0.1)
+    plt.legend()
 
 # N 人口总数
 # I 感染者
 # r 感染者接触易感者的人数
 # B 传染概率
 # y 康复概率
-#康复者将有可能再次感染
+# 康复者将有可能再次感染
 def SIS_model(N= 10000, I= 1, r= 10, B= 0.01, y= 0.02):
 
     #易感者
@@ -57,10 +58,11 @@ def SIS_model(N= 10000, I= 1, r= 10, B= 0.01, y= 0.02):
         Suspectors.append(Suspectors[idx] - r*B*Infectors[idx]*Suspectors[idx]/N + y*Infectors[idx])
         Infectors.append(Infectors[idx] + r*B*Infectors[idx]*Suspectors[idx]/N - y*Infectors[idx])
     
-    pl.plot(Suspectors,  label= u"Susceptibles")
-    pl.plot(Infectors, label= u"Infectors")
-    pl.legend()
-    pl.show()
+    plt.cla()
+    plt.plot(Suspectors,  label= u"Susceptibles")
+    plt.plot(Infectors, label= u"Infectors")
+    plt.pause(0.1)
+    plt.legend()
 
 # N 人口总数
 # I 感染者
@@ -92,11 +94,13 @@ def SIR_model(N= 10000, I= 1, r= 10, B= 0.05, y= 0.1):
         Infectors.append(Infectors[idx] + r*B*Infectors[idx]*Suspectors[idx]/N - y*Infectors[idx])
         Recovers.append(Recovers[idx] + y*Infectors[idx])
     
-    pl.plot(Suspectors,  label= u"Susceptibles")
-    pl.plot(Infectors, label= u"Infectors")
-    pl.plot(Recovers, label= u"Recovers")
-    pl.legend()
-    pl.show()
+    plt.cla()
+    plt.plot(Suspectors,  label= u"Susceptibles")
+    plt.plot(Infectors, label= u"Infectors")
+    plt.plot(Recovers, label= u"Recovers")
+    
+    plt.pause(0.1)
+    plt.legend()
 
 # N 人口总数
 # I 感染者
@@ -136,13 +140,14 @@ def SEIR_model(N= 10000, I= 1, r= 20, B= 0.03, y= 0.1, a= 0.1):
         Infectors.append(Infectors[idx] + a*Exposed[idx] - y*Infectors[idx])
         Recovers.append(Recovers[idx] + y*Infectors[idx])
     
-    pl.plot(Suspectors,  label= u"Susceptibles")
-    pl.plot(Exposed, label= u"Exposed")    
-    pl.plot(Infectors, label= u"Infectors")
-    pl.plot(Recovers, label= u"Recovers")
-
-    pl.legend()
-    pl.show()
+    plt.cla()
+    plt.plot(Suspectors,  label= u"Susceptibles")
+    plt.plot(Exposed, label= u"Exposed")    
+    plt.plot(Infectors, label= u"Infectors")
+    plt.plot(Recovers, label= u"Recovers")
+    
+    plt.pause(0.1)
+    plt.legend()
 
 # N 人口总数
 # I 感染者
@@ -182,13 +187,13 @@ def SEIR_wuhan_model(N= 10000, I= 1, r= 20, B= 0.03, r2= 20, B2= 0.03, y= 0.1, a
         Infectors.append(Infectors[idx] + a*Exposed[idx] - y*Infectors[idx])
         Recovers.append(Recovers[idx] + y*Infectors[idx])
     
-    pl.plot(Suspectors,  label= u"Susceptibles")
-    pl.plot(Exposed, label= u"Exposed")    
-    pl.plot(Infectors, label= u"Infectors")
-    pl.plot(Recovers, label= u"Recovers")
+    plt.plot(Suspectors,  label= u"Susceptibles")
+    plt.plot(Exposed, label= u"Exposed")    
+    plt.plot(Infectors, label= u"Infectors")
+    plt.plot(Recovers, label= u"Recovers")
 
-    pl.legend()
-    pl.show()
+    plt.legend()
+    plt.show()
 
 # N 人口总数
 # I 感染者
@@ -235,14 +240,14 @@ def SEIR_wuhan_blocked_model(N=10000, I= 1, r= 20,
         Infectors.append(Infectors[idx] + a*Exposed[idx] - y*Infectors[idx])
         Recovers.append(Recovers[idx] + y*Infectors[idx])
     
-    pl.figure(figsize=(12,6))
-    pl.plot(Suspectors,  label= u"Susceptibles")
-    pl.plot(Exposed, label= u"Exposed")    
-    pl.plot(Infectors, label= u"Infectors")
-    pl.plot(Recovers, label= u"Recovers")
-
-    pl.legend()
-    pl.show()
+    plt.cla()
+    plt.plot(Suspectors,  label= u"Susceptibles")
+    plt.plot(Exposed, label= u"Exposed")    
+    plt.plot(Infectors, label= u"Infectors")
+    plt.plot(Recovers, label= u"Recovers")
+    
+    plt.pause(0.1)
+    plt.legend()
     
     #print(Infectors)
     return max(Infectors), Infectors.index(max(Infectors))
@@ -301,16 +306,79 @@ def SEIR_wuhan_modified_model(N=10000, I= 1,
         Infectors.append(Infectors[idx] + a*Exposed[idx] - y*Infectors[idx] - d*Infectors[idx])
         Recovers.append(Recovers[idx] + y*Infectors[idx])
     
-    pl.figure(figsize=(12,6))
-    pl.plot(Suspectors,  label= u"Susceptibles")
-    pl.plot(Exposed, label= u"Exposed")    
-    pl.plot(Infectors, label= u"Infectors")
-    pl.plot(Recovers, label= u"Recovers")
-
-    pl.legend()
-    pl.show()
+    plt.cla()
+    plt.plot(Suspectors,  label= u"Susceptibles")
+    plt.plot(Exposed, label= u"Exposed")    
+    plt.plot(Infectors, label= u"Infectors")
+    plt.plot(Recovers, label= u"Recovers")
     
-    #print(Infectors)
+    plt.pause(0.1)
+    plt.legend()
+    
+    return max(Infectors), Infectors.index(max(Infectors))
+
+# N 人口总数
+# I 感染者
+# r 感染者接触易感者的人数
+# B 传染概率
+# y 康复概率
+# a 潜伏者转化为感染者概率
+# d 感染者死亡率
+# p 人口流入率    
+# blockday 隔离介入的时间    
+#导入潜伏期人群（潜伏期具有传染性）
+def SEIR_wuhan_model(N=10000, I= 1, 
+                     r= 20, B= 0.03, 
+                     r2= 20, B2= 0.03, 
+                     y= 0.85, a= 0.1, 
+                     d= 0.028, population= 10,
+                     bolckday= 20):
+
+    #易感者
+    S= N - I
+    
+    #复原者
+    R= 0
+    
+    #潜伏者
+    E= 0
+    
+    T = 200
+    
+    Infectors= []
+    Infectors.append(I)
+    
+    Suspectors= [] 
+    Suspectors.append(S)
+    
+    Recovers= []
+    Recovers.append(R)
+    
+    Exposed= []
+    Exposed.append(E)
+    
+    for idx in range(T):
+        if idx> bolckday:
+            r= 5
+            r2= 5
+        
+        p = 1.0 + random.randint(-1*population, population)/10000.0
+        N = N*p
+                
+        Suspectors.append(Suspectors[idx] - r*B*Infectors[idx]*Suspectors[idx]/N - r2*B2*Suspectors[idx]*Exposed[idx]/N)
+        Exposed.append(Exposed[idx] + r*B*Suspectors[idx]*Infectors[idx]/N - a*Exposed[idx] + r2*B2*Suspectors[idx]*Exposed[idx]/N)
+        Infectors.append(Infectors[idx] + a*Exposed[idx] - y*Infectors[idx] - d*Infectors[idx])
+        Recovers.append(Recovers[idx] + y*Infectors[idx])
+    
+    plt.cla()
+    plt.plot(Suspectors,  label= u"Susceptibles")
+    plt.plot(Exposed, label= u"Exposed")    
+    plt.plot(Infectors, label= u"Infectors")
+    plt.plot(Recovers, label= u"Recovers")
+    
+    plt.pause(0.1)
+    plt.legend()
+    
     return max(Infectors), Infectors.index(max(Infectors))
 
 if __name__ == '__main__':
@@ -319,20 +387,19 @@ if __name__ == '__main__':
     #SIR_model()
     #SEIR_model()
     #SEIR_wuhan_blocked_model()
-    print(SEIR_wuhan_modified_model())
+    #SEIR_wuhan_modified_model()
     
-    '''
+    plt.figure(figsize=(12,6))    
+    plt.ion()
+
     value = []
     pos= []
-    for i in range(30):
-        maxValue, maxPos = SEIR_wuhan_blocked_model(bolckday= i)
+    T= 40
+    for i in range(T):
+        maxValue, maxPos = SEIR_wuhan_model(bolckday= i)
         value.append(maxValue)
         pos.append(maxPos)
-    
-    pl.figure(figsize=(12,6))
-    #pl.plot(pos,'-og', label= u"Peak Position")
-    pl.plot(value, '-xr', label= u"Peak Value")    
-
-    pl.legend()
-    pl.show() 
-    '''
+        print(i, maxValue, maxPos)
+         
+    plt.ioff()
+    plt.show()    

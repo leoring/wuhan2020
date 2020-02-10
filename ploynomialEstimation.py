c52260c6bf8e3ac7e069b9bf99ec6b56b9806e22
leoring <lening@smmail.cn>
2020-02-10 17:54:38 +0800

    增加了线性、多项式及指数模型参数估计代码

---
+++ ploynomialEstimation.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 15:08:14 2020
+
+@author: leoring_le@hotmail.com
+"""
+
+import numpy as np
+from scipy.optimize import leastsq
+import pylab as pl
+
+def func(x, p):
+    """
+    数据拟合所用的函数: k*x+b
+    """
+    a, b, c = p
+    return a*x*x + b*x + c
+
+def residuals(p, y, x):
+    """
+    实验数据x, y和拟合函数之间的差，p为拟合需要找到的系数
+    """
+    return y - func(x, p)
+
+def SSE(p,y,x):
+    sse = 0
+    
+    xlen = len(x)
+    for i in range(xlen):
+        sse += (y[i] - func(x[i],p))*(y[i] - func(x[i],p))
+    return sse
+    
+def SST(y):
+    sst = 0
+    
+    average = np.mean(y)
+    for item in y:
+        sst += (item - average) * (item - average)
+    return sst 
+
+x= [1,2,3,4,
+    5,6,7,8,9,
+    10,11,12,13,
+    14,15,16,17,
+    18,19,20]
+
+y= [27,26,393,1118,1309,3806,
+     2077,3248,4148,4812,5019,
+     4562,5173,5072,3971,5328,
+     4833,4214,3916,4008]
+
+x = np.array(x)
+y = np.array(y)   
+
+p0 = [7, 0.2, 0.3] # 第一次猜测的函数拟合参数
+
+# 调用leastsq进行数据拟合
+# residuals为计算误差的函数
+# p0为拟合参数的初始值
+# args为需要拟合的实验数据
+plsq = leastsq(residuals, p0, args=(y, x))
+
+print (u"拟合参数", plsq[0]) # 实验数据拟合后的参数
+print(1-SSE(plsq[0],y,x)/SST(y))
+
+pl.plot(x, y, label= "Data with Noise")
+pl.plot(x, func(x, plsq[0]), label= "Fitted Value")
+pl.legend()
+pl.show()
